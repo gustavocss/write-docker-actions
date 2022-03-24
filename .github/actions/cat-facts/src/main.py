77a6e4e6eb643be6741e53e78c0bca964df28f86
gustavocss <97777141+gustavocss@users.noreply.github.com>
2022-03-24 11:50:37 -0800

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -5,14 +5,14 @@ import sys
 # Make an HTTP GET request to the cat-fact API
 cat_url = "https://cat-fact.herokuapp.com/facts"
 r = requests.get(cat_url)
-r_obj_list = r.json()
+# r_obj_list = r.json()
 
 # Create an empty list to store individual facts in
 # This will make it easy to select a random one later
 fact_list = []
 
 # Add the "text" of every object into the fact_list list
-for fact in r_obj_list:
+for fact in r:
     fact_list.append(fact["text"])
 
 # Select a random fact from the fact_list and return it
